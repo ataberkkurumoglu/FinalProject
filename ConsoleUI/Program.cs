bf855c5c64053341db159f0e47d5f022201c5378
ataberkkurumoglu <kurumoglua@gmail.com>
2021-02-03 23:47:32 +0300

    03.02.2021 ders sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
@@ -8,13 +9,13 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
-            foreach (var product in productManager.GetAll())
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetByUnitPrice(5,1000))
             {
                 Console.WriteLine(product.ProductName);
             }
 
-            Console.ReadKey();
+            
         }
     }
 }
