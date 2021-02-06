454c04bdf2ad950641f4b9e5fee07088f6bb79d4
ataberkkurumoglu <kurumoglua@gmail.com>
2021-02-06 23:57:56 +0300

    6.Şubat ders sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,15 +9,24 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            ProductTest();
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName + "  " + category.CategoryId);
+            }
         }
 
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetByUnitPrice(5, 1000))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName+"++++++"+product.CategoryName);
             }
 
         }
